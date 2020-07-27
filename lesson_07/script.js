ea4d46f6fb3f3e51e5c9cddfc024eaeec0a4dba4
Vlad <vladsgen@mail.ru>
2020-07-28 08:11:21 +1000

    callback-Функции

---
+++ lesson_07/script.js
@@ -0,0 +1,27 @@
+function first() {
+    setTimeout( function () {
+        console.log(1);
+    }, 500 );
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log("Я учу " + lang);
+    callback();
+}
+
+// learnJS("JavaScript", function () {
+//     console.log("Я прошел 3-й урок");
+// });
+
+function done() {
+    console.log("Я прошел 3-й урок");
+}
+
+learnJS("JavaScript", done);
